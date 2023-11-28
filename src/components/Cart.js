d581ae5f1cd3bc438854a905a9c4082c804db3d8
Paul-prog44 <saintouenfr@hotmail.com>
2023-11-28 15:58:36 +0100

    Add Cart

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+function Cart() {
+    const Lierre = 10;
+    const Bouquet = 15
+    const Monstera = 23
+
+    return <ul>
+                <li>Lierre</li>
+                <li>Bouquet</li>
+                <li>Monstera</li>
+                <li>Total = {Lierre + Bouquet + Monstera}</li>
+            </ul>
+}
+
+export default Cart
