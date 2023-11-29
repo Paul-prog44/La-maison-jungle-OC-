09f6700592a172b38ea36c559fc1f8bd21b5f0d6
Paul-prog44 <saintouenfr@hotmail.com>
2023-11-29 16:16:50 +0100

    Add water and light props

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,12 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
+import CareScale from'./CareScale'
 
+//Fonction d'affichage des plantes dispos
 function ShoppingList() {
 
+    //Filtre des catégories pour éviter redondance
     let categories = [];
-
     plantList.forEach(function(plant) {
         if (!categories.includes(plant.category)) {
             categories.push(plant.category);
@@ -12,16 +15,21 @@ function ShoppingList() {
 
     return (
         <div>
-        <ul>
-            {categories.map((category, index) => (
-                <li key={`${category}-${index}`}>{ category }</li>
-            ))}
-        </ul>
-        <ul>
-            {plantList.map((plant) => (
-                <li key={plant.id}>{plant.name}</li>
-            ))}
-        </ul>
+            <ul>
+                {categories.map((category, index) => (
+                    <li key={`${category}-${index}`}>{ category }</li>
+                ))}
+            </ul>
+            <ul className='lmj-plant-list'>
+                {plantList.map((plant) => (
+                    <li key={plant.id} className="lmj-plant-item">
+                        {plant.name}
+                        <CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />                    </li>
+                    
+                    
+                ))}
+            </ul>
         </div>
         )
             
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -3,54 +3,65 @@ export const plantList = [
 		name: 'monstera',
 		category: 'classique',
 		id: '1ed',
-		isBestSale : false
+		isBestSale: true,
+		light: 2,
+		water: 3
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
-		isBestSale : true
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-		isBestSale : false
+		light: 1,
+		water: 2
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
-		isBestSale : false
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-		isBestSale : false
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
 		id: '6uo',
-		isBestSale :true
+		light: 2,
+		water: 2
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isBestSale : false
+		isBestSale: true,
+		light: 2,
+		water: 3
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-		isBestSale : false
+		light: 2,
+		water: 1
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
-		isBestSale : true
+		light: 2,
+		water: 1
 	}
 ]
