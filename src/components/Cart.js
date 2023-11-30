394436dd68dfa04e78d96f445d96ad485ae111a5
Paul-prog44 <saintouenfr@hotmail.com>
2023-11-30 17:07:16 +0100

    Add open/close cart option, restructure elements

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,27 @@
 import "../styles/Cart.css"
+import { useState } from "react"
 
 function Cart() {
-    const Lierre = 10;
-    const Bouquet = 15
-    const Monstera = 23
+    const monsteraPrice = 8
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
 
-    return <div className="lmj-cart">
-                <h2>Panier</h2>
-                <ul>
-                    <li>Lierre</li>
-                    <li>Bouquet</li>
-                    <li>Monstera</li>
-                </ul>
-                <p>Total = {Lierre + Bouquet + Monstera}</p>
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
+            <h2>Panier</h2>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={()=> updateCart(cart +1)}>
+                    Ajouter
+                </button>
             </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+        </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+    )
+
 }
 
 export default Cart
