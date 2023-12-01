fe086c45d934af547ea6daf235da8e93e87100ba
Paul-prog44 <saintouenfr@hotmail.com>
2023-12-01 15:44:47 +0100

    Add plant images

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,4 +1,13 @@
 import monstera from '../assets/monstera.jpg'
+import mint from '../assets/mint.jpg'
+import lyrata from '../assets/lyrata.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
+import basil from '../assets/basil.jpg'
+import pothos from '../assets/pothos.jpg'
+import olivier from '../assets/olivier.jpg'
+import succulent from '../assets/succulent.jpg'
+
 
 export const plantList = [
 	{
@@ -12,76 +21,76 @@ export const plantList = [
 		happiness: 2
 	},
 	{
-		name: 'ficus lyrata',
+		name: 'mint',
 		category: 'classique',
 		id: '2ab',
 		light: 3,
 		water: 1,
-		cover: monstera,
+		cover: mint,
 		happiness: 2
 	},
 	{
-		name: 'pothos argenté',
+		name: 'lyrata',
 		category: 'classique',
 		id: '3sd',
 		light: 1,
 		water: 2,
-		cover: monstera,
+		cover: lyrata,
 		happiness: 2
 	},
 	{
-		name: 'yucca',
+		name: 'calathea',
 		category: 'classique',
 		id: '4kk',
 		light: 3,
 		water: 1,
-		cover: monstera,
+		cover: calathea,
 		happiness: 1
 	},
 	{
-		name: 'olivier',
+		name: 'cactus',
 		category: 'extérieur',
 		id: '5pl',
 		light: 3,
 		water: 1,
-		cover: monstera,
+		cover: cactus,
 		happiness: 3
 	},
 	{
-		name: 'géranium',
+		name: 'basil',
 		category: 'extérieur',
 		id: '6uo',
 		light: 2,
 		water: 2,
-		cover: monstera,
+		cover: basil,
 		happiness: 3
 	},
 	{
-		name: 'basilique',
+		name: 'pothos',
 		category: 'extérieur',
 		id: '7ie',
 		isBestSale: true,
 		light: 2,
 		water: 3,
-		cover: monstera,
+		cover: pothos,
 		happiness: 3
 	},
 	{
-		name: 'aloe',
+		name: 'olivier',
 		category: 'plante grasse',
 		id: '8fp',
 		light: 2,
 		water: 1,
-		cover: monstera,
+		cover: olivier,
 		happiness: 2
 	},
 	{
-		name: 'succulente',
+		name: 'succulent',
 		category: 'plante grasse',
 		id: '9vn',
 		light: 2,
 		water: 1,
-		cover: monstera,
+		cover: succulent,
 		happiness: 3
 	}
 ]
